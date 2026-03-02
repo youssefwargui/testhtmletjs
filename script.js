1c72e5b3f5bfe33bfc5be386d805f5680c87c21d
youssefwargui <youssefwargui100@gmail.com>
2026-03-02 21:08:42 +0100

    ex

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ console.log(text[0].innerHTML , "text");
 
 var image = document.getElementById("x")
 console.log(image );
+
+var btn = document.getElementById("mybutton")
+
+button.addEventListener("click", function() {
+    img.src = "http://netdna.webdesignerdepot.com/uploads/2013/04/Dvdp3.gif"
+  })
+
+ 
